9b7f33564df4d23dd301ed53c5e7595de88d2314
SARATh _S <kkayyess@gmail.com>
2022-07-01 12:02:55 +0530

    asset indaki

--- now/static/js/src/advantage/react/hooks/useStripePublishableKey.ts
+++
@@ -1,9 +0,0 @@
-import type { StripePublishableKey } from "advantage/api/types";
-import { useQuery } from "react-query";
-
-export const useStripePublishableKey = () => {
-  const { data: stripePublishableKey } = useQuery<StripePublishableKey>(
-    "stripePublishableKey"
-  );
-  return { stripePublishableKey };
-};
