8cbe0a0836d5037c7184b1c55127e3c75bc59151
Sarath S <95010659+sarathselvan@users.noreply.github.com>
2025-05-16 15:56:56 +0530

    Update script.js

--- lightmode2/script.js
+++ lightmode2/script.js
@@ -13,9 +13,9 @@ document.getElementById('contact-form').addEventListener('submit', function(e) {
   alert('Thank you for reaching out!');
   this.reset();
 });
-const cursorTracker = document.getElementById('cursor-tracker');
-document.addEventListener('mousemove', (e) => {
+//const cursorTracker = document.getElementById('cursor-tracker');
+//document.addEventListener('mousemove', (e) => {
     // Update the cursor tracker's position
-    cursorTracker.style.left = `${e.pageX - 10}px`;
-    cursorTracker.style.top = `${e.pageY - 10}px`;
-});
+ //   cursorTracker.style.left = `${e.pageX - 10}px`;
+//    cursorTracker.style.top = `${e.pageY - 10}px`;
+//}); 
