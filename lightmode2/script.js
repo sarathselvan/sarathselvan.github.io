e240e5309072e9c975f9fbfd0585358a75af994d
Sarath S <kkayyess@gmail.com>
2025-05-29 09:06:13 +0530

    Update script.js

--- lightmode2/script.js
+++ lightmode2/script.js
@@ -8,14 +8,3 @@ document.querySelectorAll('.nav-links a').forEach(link => {
   });
 });
 
-document.getElementById('contact-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-  alert('Thank you for reaching out!');
-  this.reset();
-});
-const cursorTracker = document.getElementById('cursor-tracker');
-document.addEventListener('mousemove', (e) => {
-   Update the cursor tracker's position
-    cursorTracker.style.left = `${e.pageX - 10}px`;
-    cursorTracker.style.top = `${e.pageY - 10}px`;
-}); 
